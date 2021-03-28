df28db3a61be29d009aec6e8d3afeeac0c9eef88
Priyanka <priyanka.23219@gmail.com>
2021-03-28 12:22:38 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
+import { Section3Component } from './section3/section3.component';
+import { Section4Component } from './section4/section4.component';
+import { Section5Component } from './section5/section5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    Section4Component,
+    Section5Component
   ],
   imports: [
     BrowserModule,
